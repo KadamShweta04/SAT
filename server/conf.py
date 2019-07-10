637d162228a9951aa0de2d98ae588dade82abb4f
mirco <12728527+mircohaug@users.noreply.github.com>
2019-07-10 15:59:09 +0200

    configured rest of latex template

--- server/conf.py
+++ server/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
 
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- Project information -----------------------------------------------------
 
@@ -23,7 +23,6 @@ author = 'Mirco Haug'
 
 # The full version, including alpha/beta/rc tags
 release = '1.0.0'
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -43,7 +42,6 @@ templates_path = ['sphinx-doc/_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -62,26 +60,15 @@ numfig = True
 
 
 latex_elements = {
-    'passoptionstopackages':"""
-        \\textwidth 14cm
-        \\textheight 22cm
-        \\topmargin 0.0cm
-        \evensidemargin 1cm
-        \oddsidemargin 1cm
-        \parskip0.5explus0.1exminus0.1ex
-        \\usepackage{epsf}
-        \\usepackage{graphics, graphicx}
-        \\usepackage{latexsym}
-        \\usepackage[margin=10pt,font=small,labelfont=bf]{caption}
-        \\usepackage[utf8]{inputenc}
-        \\usepackage[toc,page]{appendix}
+    'preamble': """
+\\textwidth 14cm
+\\textheight 22cm
+\\topmargin 0.0cm
+\\evensidemargin 1cm
+\\oddsidemargin 1cm
+\\setlength\parindent{20pt}
     """,
-    'fncychap':'\\usepackage{fncychap}',
-    'fontpkg':'\\usepackage{amsmath,amsfonts,amssymb,amsthm}',
-    'preamble' : """
-    \sloppy
-    """,
-    'maketitle':"""
+    'maketitle': """
 \\begin{titlepage}
 	\makeatletter
 	\\begin{center}
@@ -112,11 +99,71 @@ Universität Tübingen
 \makeatother
 \end{titlepage}
     """,
+    'fncychap': '',
+    'tableofcontents': """
+    
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+  
+\\pagenumbering{roman}
+\\setcounter{page}{1}
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Seite I: Zusammenfassug, Danksagung
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+
+\\section*{Abstract}
+
+During the course of this research project an application to compute linear layouts of graphs was created. This application provides a wide variety of options and constraints to force the resulting layout in the desired shape. The application in based on python and flask. The actual computing is done by creating SAT instances from the given problem and pass those instances to the lingeling SAT solver. The application provides a extensivly documented REST API to accept problem instances. This API is currently only used by the frontend located at. 
+
+\\section*{Zusammenfassung}
+
+Bei einer englischen Masterarbeit muss zus\\"atzlich eine deutsche Zusammenfassung verfasst werden.
+
+\\cleardoublepage
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Table of Contents
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+\\renewcommand{\\baselinestretch}{1.3}
+\\small\\normalsize
+
+\\tableofcontents
+
+\\renewcommand{\\baselinestretch}{1}
+\\small\\normalsize
+
+\\cleardoublepage
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% List of Figures
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+\\renewcommand{\\baselinestretch}{1.3}
+\\small\\normalsize
+
+\\addcontentsline{toc}{chapter}{List of Figures}
+\\listoffigures
+
+\\renewcommand{\\baselinestretch}{1}
+\\small\\normalsize
+
+\\cleardoublepage
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% Der Haupttext, ab hier mit arabischer Numerierung
+%%% Mit \\input{dateiname} werden die Datei `dateiname' eingebunden
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+\\pagenumbering{arabic}
+\\setcounter{page}{1}
+""",
     'pointsize': '14pt',
     'papersize': 'a4paper',
+    'sphinxsetup': 'TitleColor={rgb}{0.,0.,0.}',
 
 }
-#TODO latex stuff
-# * Title
-# * style
-# * Abstract deu / eng
+
+master_doc = 'index'
+latex_documents = [(master_doc, 'main.tex', project, author, 'report')]
