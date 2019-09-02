c1092cac8cec3f380610357db933883e557c9fc2
juliamaennecke <julia.mae@gmx.de>
2019-09-02 13:11:05 +0200

    thesis update

--- client/Thesis/Examplecodes/example4.js
+++ client/Thesis/Examplecodes/example4.js
@@ -3,14 +3,16 @@ var graph = {
 	edges: 27,
 };
 
+// adding an event listener to a button that is triggered by hovering
 $("#button").click(function() {
 	$("#button").style.border = "1px solid red";
 })
 
+// adding an event listener to a button that is triggered by click
 $("#button").click(function() {
 	var string = 	"This graph has " + graph.nodes.toString() +
-					" nodes and " + graph.edges.toString() + " edges"
-	alert(string)
+					" nodes and " + graph.edges.toString() + " edges";
+	alert(string);
 })
 
 
